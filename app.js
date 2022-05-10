cac68a1555417f237a47efeac291c88f41a954c9
Ashley Douglas <douglasashleyc@gmail.com>
2022-05-09 22:06:22 -0400

    updates to button color, start function, round state,

--- app.js
+++ app.js
@@ -3,80 +3,100 @@
 // const startButton = document.getElementById('timer')
 // const questionContainerElement = document.getElementById('questionsContainer')
 
+
+
+
 // startButton.addEventListener("click",startGame)
 const startButton = document.getElementById('timer') 
-const startButton_questions = document.getElementById('startButton')
-const questionsContainerElement = document.getElementById('questionsContainer')
-const answers = document.querySelectorAll('#answersButton')
-// const answerButtonsElement = document.getElementById("answerbutton")
-const songvideos = document.querySelectorAll('.musicBox')
-const videoArray = ["https://www.youtube.com/watch?v=jQ-RrGCSa2M","https://www.youtube.com/watch?v=UmPgMc3R8zg", "https://www.youtube.com/watch?v=t7SvtikTkrM","https://www.youtube.com/watch?v=B95OUKk7alM","https://www.youtube.com/watch?v=oYpcCMxQXaE", "https://www.youtube.com/watch?v=5nj1HWC-dQs","https://www.youtube.com/watch?v=l3S1naGY9EQ"]
-const scores = document.querySelectorAll('.scores')
 const timer = document.querySelector('#timer')
-const video = document.createElement('video');
-video.src = videoArray
-// video.poster ='';
-video.autoplay = false;
-video.controls = true;
-video.muted = false;
-video.height = 100; // 
-video.width = 200; // 
+const player1turn = document.querySelector('#playBtn1')
+const answerbuttonWrong = document.querySelector("#false");
+const answerbuttonRight = document.querySelector('#true');
 
-if (video.canPlayType('video/mp4')) {
-  console.log('set src to mp4 video');
-
-  video.src = 'my-video.mp4'
-} else if (video.canPlayType('video/ogg')) {
-  console.log('set src to ogg video');
-
-  video.src = 'my-video.ogg'
-} else {
-  console.log('provide link to user');
-}
-
-const box = document.getElementById('musicBox');
-
-box.appendChild(video);
-
-
-let Player1 = 'Player 1'
-let Player2 = 'Player 2'
-let players = 0;
-let questionPrompt = [];
-let correctanswer;
-let player1score = 0;
-let player2score = 0;
+answerbuttonWrong.addEventListener('click', () => answerbuttonWrong.style.backgroundColor='red');
+answerbuttonRight.addEventListener('click', () => answerbuttonRight.style.backgroundColor='green');
+// let player1ScoreCount = document.getElementById('.p1score-count')
+// let player2ScoreCount = document.getElementById('.p2score-count')
 let timerInterval;
-// startButton_questions.addEventListener('click', startGameAndTimer)
+
+// How to choose correct answer on button and add score to specific player
+const answerQuestion1 = document.getElementById('.button')
+const answerQuestion2 = document.getElementById('.button')
+const answerQuestion3 = document.getElementById('.button')
+const answerQuestion4 = document.getElementById('.button')
+const answerQuestion5 = document.getElementById('.button')
+const answerQuestion6 = document.getElementById('.button')
+const answerQuestion7 = document.getElementById('.button')
+
+// if(document.getElementById('answerbutton-true')){
+//     player1ScoreCount += 10;
+//     alert("button was clicked");
+// }
+//activate player turn on buttons, will insert score into specific score element
+
+let playerone = {
+    score: 0,
+    rightanswers: [],
+    wronganswers: [],
+    round: 0,
+    isTurn: true
+
+}
 
 
 
-startGame.forEach(button => {
-    button.onclick = () => {
-        players = button.innerHTML.charAt(0)
-        setGame()
-
-    }
-})
 
 
-const setGame = () => {
-    if (players === '1'){
-        console.log("Player 1 turn")
+
+// const addPoints = () => {
+
+     let points = 0
+
+     function addPoints(){
+        let player1ScoreCount = document.querySelector('.totalPointp1')
+         playerone.score+= 10
+         console.log(playerone.score)
+         playerone.rightanswers.push(playerone.score)
+         player1ScoreCount.innerHTML = playerone.score
         
-    }
 }
 
-const askQuestions = () => {
-    function showQuestion(){
-        questionsContainerElement.innerText = questions.question
-    
-    
-    }
-//    for(let i = 0; i < videoArray.length, i++;)
+
+
+     function removePoints(){
+        let player1ScoreCount = document.querySelector('.totalPointp1')
+         playerone.score -= 10
+         console.log(playerone.score)
+         playerone.wronganswers.push(playerone.score)
+         player1ScoreCount.innerHTML = playerone.score;
+         
+         
 }
 
- startGameAndTimer = () => {
+const answerWasChosen = (e) => { 
+    // alert("button was clicked");
+    const answerQuestion1 = document.getElementById('')
+    const answerQuestion2 = document.getElementById('')
+    const answerQuestion3 = document.getElementById('')
+    const answerQuestion4 = document.getElementById('')
+    const answerQuestion5 = document.getElementById('')
+    const answerQuestion6 = document.getElementById('')
+    const answerQuestion7 = document.getElementById('')
+    if(e.target.classList.contains('answerbutton-true')){
+    player1ScoreCount += 10 ;
+    // alert("hello");
+    }
+}
+ const answers = document.getElementsByClassName('button')  
+for (let i=0; i<answers.length; i++){
+    
+    answers[i].addEventListener("click", answerWasChosen)
+    
+}
+
+
+///Start the timer and player ones turn
+const startGameAndTimer = () => {
     //Clear the time for the new round
     clearInterval(timerInterval);
     /// clear the m,s,h
@@ -98,173 +118,171 @@ const askQuestions = () => {
         second++;
 
         // if second equals 60 (one minute)
-        if (second ==60){
+        if (second = 60){
             // a minute has passed so reset seconds to 0
             minute++;
             second = 0;
         }
 
-        // if minute goes to 2 then we reset the minutes to 0
-        if (minute == 3){
-            minute = 0;
-            console.log("Next Player's Turn!")
+        // if minute goes to 1 then we reset the minutes to 0
+        if (minute === 1){
+            minute ===0;
+            /// start player 2 turn after 1 minute
+            
         }
 
     }, 1000);
+    function greet() {
+        alert('Next Round!');
+       }
+       setTimeout(greet, 60000);
+    }
 
 
+
+
+const changeRound = (num) => {
+        let clock = document.querySelector('.roundBox')
+        clock.append(playerone.round)
+    
+    }
 
     
-    
 
-
-    
-
-
-
-    // startButton.classList.add('hide')
-    // questionContainerElement.classList.remove('hide')
-    // goToNextQuestion()
-    // startTimer()
-    
-/// player clicks to next question with next question button
-
-
-function startNextQuestion(){
-    showQuestion()
-  
+const restartRound = () => {
+   if (player1ScoreCount <=10){
+       startGameAndTimer;
+   } else if (player1ScoreCount => 20){
+       changeRound(2)
+   }
 }
 
 
+const setRounds = () =>{
 
-function answerQuestion(){
+    if (playerone.score <= 19){
+        changeRound(1)
+
+    }
+
+    if (playerone.score >= 20) {   
+        changeRound(2)
+    }
+    else if(playerone.score < 20){
+       
+        restartRound();
+    }
+
+    else if(playerone.score >= 30){
+
+        changeRound(3)
+    }
+}
+
  /// if answer is correct, score increases and meme populates behind gameboard
     /// if answer is incorect no points are added     
-}
-  
+ 
 
 
 /// Questions should this be a class or an array?
-const questions = {
-                ///Round One
-        question: "Which song samples this J Dilla Produced Hit?",
-         song: ('https://www.youtube.com/watch?v=jQ-RrGCSa2M'),
-            answers: [
-            {text: 'A Tribe Called Quest - Can I Kick It', correct: false},
-            {text: 'Mya - Fallen', correct: true},
-            {text: 'Sade - Your Love is King', correct: false},
-            {text: 'N.E.R.D. - Love Bomb', correct: false},
+// const questions = {
+//                 ///Round One
+//         question: "Which song samples this J Dilla Produced Hit?",
+//          song: ('https://www.youtube.com/watch?v=jQ-RrGCSa2M'),
+//             answers: [
+//             {text: 'A Tribe Called Quest - Can I Kick It', correct: false},
+//             {text: 'Mya - Fallen', correct: true},
+//             {text: 'Sade - Your Love is King', correct: false},
+//             {text: 'N.E.R.D. - Love Bomb', correct: false},
            
-        ],
+//         ],
 
-        question: "Which popular R&B Dance Pop Song Sampled this Stevie Knicks Song?",
-            song: ('https://www.youtube.com/watch?v=UmPgMc3R8zg'),
+//         question: "Which popular R&B Dance Pop Song Sampled this Stevie Knicks Song?",
+//             song: ('https://www.youtube.com/watch?v=UmPgMc3R8zg'),
 
-        answers: [
-            {text: 'Britney Spears - Baby One More Time', correct: false},
-            {text: 'Jennifer Lopez - On the Floor', correct: false},
-            {text: 'Usher - DJ Got us Fallin in Love', correct: false},
-            {text: 'Destinys Child - Bootylicious', correct: true},
-        ],
-
-
-        question: "This Song Was Sampled by Which this 2007 Hit ",
-        song:('https://www.youtube.com/watch?v=t7SvtikTkrM'),
-        answers: [
-            {text: 'Timbaland - The Way I Are', correct:false},
-            {text: 'M.I.A. - Paper Planes', correct: true},
-            {text: 'Shop Boyz - Party Like a Rockstar', correct: false},
-            {text: 'Rihanna - Dont Stop the Music', correct: false},
-        ],
-                        ///Round 2
+//         answers: [
+//             {text: 'Britney Spears - Baby One More Time', correct: false},
+//             {text: 'Jennifer Lopez - On the Floor', correct: false},
+//             {text: 'Usher - DJ Got us Fallin in Love', correct: false},
+//             {text: 'Destinys Child - Bootylicious', correct: true},
+//         ],
 
 
-        question: "Kanye West's 'Touch the Sky' sampled which classic song",
-        song:('https://www.youtube.com/watch?v=B95OUKk7alM'),
-        answers: [
-            {text: 'Stevie Wonder - Living For the City', correct: false},
-            {text: 'Skys the limit - Notorious B.I.G.', correct: false},
-            {text: 'Curtis Mayfield - Move On Up', correct: true},
-            {text: 'Sittin On the Dock of the Bay - Otis Redding', correct:false},
-        ],
+//         question: "This Song Was Sampled by Which this 2007 Hit ",
+//         song:('https://www.youtube.com/watch?v=t7SvtikTkrM'),
+//         answers: [
+//             {text: 'Timbaland - The Way I Are', correct:false},
+//             {text: 'M.I.A. - Paper Planes', correct: true},
+//             {text: 'Shop Boyz - Party Like a Rockstar', correct: false},
+//             {text: 'Rihanna - Dont Stop the Music', correct: false},
+//         ],
+//                         ///Round 2
 
 
-        question: "Which artist's song features this Stevie Wonder sample?",
-        song:('https://www.youtube.com/watch?v=oYpcCMxQXaE'),
-        answers: [
-            {text: 'Frank Ocean - Sweet Life', correct:true},
-            {text: 'Beyonce - Summertime', correct: false},
-            {text: 'Jay Z - Girls Girls Girls', correct: false},
-            {text: 'Outkast - Otis Redding', correct: false},
-        ],
+//         question: "Kanye West's 'Touch the Sky' sampled which classic song",
+//         song:('https://www.youtube.com/watch?v=B95OUKk7alM'),
+//         answers: [
+//             {text: 'Stevie Wonder - Living For the City', correct: false},
+//             {text: 'Skys the limit - Notorious B.I.G.', correct: false},
+//             {text: 'Curtis Mayfield - Move On Up', correct: true},
+//             {text: 'Sittin On the Dock of the Bay - Otis Redding', correct:false},
+//         ],
 
 
-        question: "This song is sampled by which Hip Hop classic?",
-         song: ('https://www.youtube.com/watch?v=5nj1HWC-dQs'), 
-        answers: [
-            {text: 'Camp Lo - Luchini (This is it)', correct:false},
-            {text: 'Digable Planets - Rebirth of Slick (Cool like dat)', correct: false},
-            {text: 'The Fugees - Ready or Not', correct: false},
-            {text: 'A Tribe Called Quest - Electric Relaxation', correct: true},
-        ],
+//         question: "Which artist's song features this Stevie Wonder sample?",
+//         song:('https://www.youtube.com/watch?v=oYpcCMxQXaE'),
+//         answers: [
+//             {text: 'Frank Ocean - Sweet Life', correct:true},
+//             {text: 'Beyonce - Summertime', correct: false},
+//             {text: 'Jay Z - Girls Girls Girls', correct: false},
+//             {text: 'Outkast - Otis Redding', correct: false},
+//         ],
 
-        ///// Bonus?
 
-        question: "Name This Sample",
-        song: ('https://www.youtube.com/watch?v=l3S1naGY9EQ'),
-        answers: [
-            {text: "De La Soul - Stakes Is High", correct: true},
-            {text: "Mos Def - Mathematics", correct: false}
-        ]
+//         question: "This song is sampled by which Hip Hop classic?",
+//          song: ('https://www.youtube.com/watch?v=5nj1HWC-dQs'), 
+//         answers: [
+//             {text: 'Camp Lo - Luchini (This is it)', correct:false},
+//             {text: 'Digable Planets - Rebirth of Slick (Cool like dat)', correct: false},
+//             {text: 'The Fugees - Ready or Not', correct: false},
+//             {text: 'A Tribe Called Quest - Electric Relaxation', correct: true},
+//         ],
+
+//         ///// Bonus?
+
+//         question: "Name This Sample",
+//         song: ('https://www.youtube.com/watch?v=l3S1naGY9EQ'),
+//         answers: [
+//             {text: "De La Soul - Stakes Is High", correct: true},
+//             {text: "Mos Def - Mathematics", correct: false}
+//         ]
 
 
     
-}
-///(answer to questions on who sampled artist name and year)
+// }
+// ///(answer to questions on who sampled artist name and year)
 
 
-function startTimer(){
-/// timer is 30 seconds as soon as music is played should thi be included in my start game function?
+// // function startTimer(){
+// // /// timer is 30 seconds as soon as music is played should thi be included in my start game function?
 
-}
-
-
-
-
-
-
-/// if player wins a round then their name populates with confetti and meme background
-
-
-
-
-/// log score of player one if they get it right or wrong
-
-///player one has 3 questions in round one
-
-
-/// when 3 questions end, prompt will come up say it ended, how do I make it not work after round?
-
-/// after 3 questions, round one player two starts when they press the start button
-
-
-/// repeat process until the end of round one
-
-//prompt will pop up with final round one scores
-
-
-/// Users can replay round one or move on to round two
+// // }
 
 
 
 
 
-// If they click yes to move on to round two, It automatically starts next round
 
-/// music sample pops up 
+// /// if player wins a round then their name populates with confetti and meme background
 
 
-//// player one chooses answers with button from choices
+
+
+// /// log score of player one if they get it right or wrong
+
+
+
+// //prompt will pop up with final round one scores
 
 
 
